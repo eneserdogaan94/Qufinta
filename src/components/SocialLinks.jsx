70720cfd5d794b413a3a034eaf6bb39219c62e8e
Enes Erdoğan <eneserdogaan94@gmail.com>
2023-05-30 13:48:50 +0300

    Fixed SocialLink

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -9,7 +9,7 @@ const SocialLinks = () => {
       id: 1,
       child: (
         <>
-          Instagram <FaInstagram size={30} />
+           Instagram <FaInstagram size={30} />
         </>
       ),
       href: "https://www.instagram.com/qufinta/",
@@ -33,7 +33,7 @@ const SocialLinks = () => {
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-white" +
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-[#00bf62]" +
               " " +
               style
             }
